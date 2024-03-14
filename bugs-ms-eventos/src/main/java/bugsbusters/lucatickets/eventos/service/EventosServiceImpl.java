6874b0fd02cd6d2544876d58314139cf6cfde2bc
Andriu89 <andrestercero89@gmail.com>
2024-03-14 11:08:01 +0100

    listado y añadir de EventosService

---
+++ bugs-ms-eventos/src/main/java/bugsbusters/lucatickets/eventos/service/EventosServiceImpl.java
@@ -0,0 +1,37 @@
+package bugsbusters.lucatickets.eventos.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+/**
+ * EventosServiceImpl
+ * Es la implementación de la interfaz service para pasar las métodos de la aplicación
+ * 14/03/2024
+ * V1
+ * bugs-ms-eventos
+ */
+public class EventosServiceImpl implements EventosService{
+	
+	@Autowired
+	private EventosRepository eventosRepository;
+	
+	/**
+	 * Método para mostrar la lista de eventos guardados en la base de datos
+	 */
+	@Override
+	public List<Evento> listadoEventos() {
+		return eventosRepository.findAll();
+	}
+	
+	/**
+	 * Método para añadir un evento en la base de datos
+	 */
+	@Override
+	public Evento anadirEvento(Evento evento) {
+		Evento eventoGuardado = eventosRepository.save(evento);
+		
+		return eventoGuardado;
+	}
+
+}
