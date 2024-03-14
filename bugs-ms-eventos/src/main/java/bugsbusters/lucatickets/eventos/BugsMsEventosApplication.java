fde54d0a2b9424cda1e174b5cf8419ad62b1affd
Arnau Gonzalez <arnaugonzalez>
2024-03-14 12:46:50 +0100

    clients and server connected carcasas

--- bugs-ms-eventos/src/main/java/bugsbusters/lucatickets/eventos/BugsMsEventosApplication.java
+++ bugs-ms-eventos/src/main/java/bugsbusters/lucatickets/eventos/BugsMsEventosApplication.java
@@ -2,8 +2,10 @@ package bugsbusters.lucatickets.eventos;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class BugsMsEventosApplication {
 
 	public static void main(String[] args) {
