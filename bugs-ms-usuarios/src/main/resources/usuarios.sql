6aec3190b0e2ce87af96e40f79bb9c2526f39aac
Andriu89 <andrestercero89@gmail.com>
2024-03-15 09:32:37 +0100

    cambios prueba

--- bugs-ms-usuarios/src/main/resources/usuarios.sql
+++ bugs-ms-usuarios/src/main/resources/usuarios.sql
@@ -3,7 +3,7 @@ CREATE TABLE `usuarios` (
    `nombre` varchar(45) NOT NULL,
   `apellido` varchar(45) NOT NULL,
   `email` varchar(45) NOT NULL,
-  `contraseña` varchar(45) NOT NULL,
+  `contrasena` varchar(45) NOT NULL,
   `fecha_alta` varchar(15) NOT NULL,
   PRIMARY KEY (`id_usuario`)
 ) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8mb4;
