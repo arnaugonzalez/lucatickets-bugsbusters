b24f86d50619f6f8de03c43211e5722fbdae6bc4
mireiasuefra <mireia.suefra@gmail.com>
2024-03-18 15:24:23 +0100

    cambio pequeñito (borrar linea)

--- bugs-ms-pagos/src/main/java/bugsbusters/lucatickets/pagos/feignclients/EventoFeignClient.java
+++ bugs-ms-pagos/src/main/java/bugsbusters/lucatickets/pagos/feignclients/EventoFeignClient.java
@@ -5,7 +5,6 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-import bugsbusters.lucatickets.pagos.model.Evento;
 import bugsbusters.lucatickets.pagos.model.response.EventoResponse;
 
 /**
