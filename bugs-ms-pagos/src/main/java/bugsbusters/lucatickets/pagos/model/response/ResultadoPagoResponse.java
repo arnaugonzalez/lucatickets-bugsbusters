d2a8a4210bdd804b7e3fcc1e839050f5b46dbf6b
mireiasuefra <mireia.suefra@gmail.com>
2024-03-18 12:56:04 +0100

    añadidos cambios para solucionar errores

--- bugs-ms-pagos/src/main/java/bugsbusters/lucatickets/pagos/model/response/ResultadoPagoResponse.java
+++ bugs-ms-pagos/src/main/java/bugsbusters/lucatickets/pagos/model/response/ResultadoPagoResponse.java
@@ -2,6 +2,7 @@ package bugsbusters.lucatickets.pagos.model.response;
 
 import java.io.Serializable;
 
+
 import bugsbusters.lucatickets.pagos.model.Pago;
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -13,6 +14,10 @@ import lombok.NoArgsConstructor;
  * Clase que representa la respuesta de un pago.
  * Esta clase proporciona una estructura de datos para la respuesta de un pago, incluyendo información como la marca de tiempo, estado, mensaje de error, información adicional, etc.
  */
+
+@NoArgsConstructor
+@Data
+@AllArgsConstructor
 public class ResultadoPagoResponse implements Serializable {
 	private static final long serialVersionUID = 1L;
 	
