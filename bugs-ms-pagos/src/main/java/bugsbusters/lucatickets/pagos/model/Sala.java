dd6abcd5766a3ff82c873ae51193e6660c677d60
AdrianMontes <adrian.montes01@estudiant.upf.edu>
2024-03-18 11:33:21 +0100

    Añadidas las clases de model y response básicas

---
+++ bugs-ms-pagos/src/main/java/bugsbusters/lucatickets/pagos/model/Sala.java
@@ -0,0 +1,70 @@
+package bugsbusters.lucatickets.pagos.model;
+
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.Positive;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+/**
+ * Clase que representa una sala en el sistema LucaTickets. Esta clase contiene
+ * información sobre una sala, como su nombre, ciudad, dirección, tipo de
+ * recinto, aforo, etc.
+ */
+
+@Entity
+@Table(name = "sala")
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+public class Sala {
+
+	/**
+	 * Identificador único de la sala.
+	 */
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "id_sala")
+	private Long id;
+
+	/**
+	 * El nombre de la sala.
+	 */
+	@NotEmpty(message = "El nombre no puede estar vacío")
+	private String nombre;
+
+	/**
+	 * La ciudad donde se encuentra la sala.
+	 */
+	@NotEmpty(message = "Falta una ciudad establecida")
+	private String ciudad;
+
+	/**
+	 * La dirección de la sala.
+	 */
+	@NotEmpty(message = "No se ha introducido una dirección")
+	private String direccion;
+
+	/**
+	 * El tipo de recinto de la sala.
+	 */
+	@NotEmpty(message = "Tiene que haber un tipo de recinto")
+	private String tipo_recinto;
+
+	/**
+	 * El aforo de la sala.
+	 */
+	@NotEmpty(message = "Aforo es nulo. Pon un valor correcto")
+	@Positive(message = "El valor del aforo tiene que ser positivo")
+	@Min(value = 1, message = "El aforo tiene que ser como mínimo 1")
+	private int aforo;
+}
