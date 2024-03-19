2ba0bf7f0215dda60ca76c35c7e2daaad8fd4870
mireiasuefra <mireia.suefra@gmail.com>
2024-03-19 13:24:08 +0100

    pull en main para actualizar mi local

--- bugs-ms-pagos/src/test/java/bugsbusters/lucatickets/pagos/BugsMsPagosApplicationTests.java
+++ bugs-ms-pagos/src/test/java/bugsbusters/lucatickets/pagos/BugsMsPagosApplicationTests.java
@@ -19,6 +19,10 @@ import jakarta.validation.Validation;
 import jakarta.validation.Validator;
 import jakarta.ws.rs.core.MediaType;
 
+
+/**
+ * Clase que contiene pruebas unitarias para la aplicación de pagos.
+ */
 @SpringBootTest
 @AutoConfigureMockMvc
 class BugsMsPagosApplicationTests {
@@ -28,30 +32,50 @@ class BugsMsPagosApplicationTests {
 
 	private Validator validator; 
 
+    /**
+     * Configura el validador antes de cada prueba.
+     */
 	@BeforeEach 
 	public void setUp() {
         validator = Validation.buildDefaultValidatorFactory().getValidator();
     }
 
+    /**
+     * Prueba para asegurarse de que el contexto de la aplicación se carga correctamente.
+     */
 	@Test
 	void contextLoads() {
 	}
 
+    /**
+     * Prueba para validar que el nombre del titular de la tarjeta no esté vacío.
+     * Si el nombre del titular de la tarjeta está vacío, debe haber una única violación de restricción.
+     *
+     * @param pago El objeto Pago a validar.
+     * @return El número de violaciones de restricción encontradas.
+     */
 	 @Test
-	    public void testNombreTitularVacio() {
-	        // Creamos un objeto Pago con el campo nombreTitular vacío
-	        Pago pago = new Pago("", "4242424242424242", 12, 2025, 123, "VISA", "Compra en línea", 100.00);
+	  public void testNombreTitularVacio() {
+	      // Creamos un objeto Pago con el campo nombreTitular vacío
+	      Pago pago = new Pago("", "4242424242424242", 12, 2025, 123, "VISA", "Compra en línea", 100.00);
 	        
-	        // Validamos el objeto Pago y esperamos que haya una única violación de restricción
-	        Set<ConstraintViolation<Pago>> violations = validator.validate(pago);
-	        assertEquals(1, violations.size());
+	      // Validamos el objeto Pago y esperamos que haya una única violación de restricción
+	      Set<ConstraintViolation<Pago>> violations = validator.validate(pago);
+	      assertEquals(1, violations.size());
 	        
-	        // Verificamos que el mensaje de error es el esperado
-	        ConstraintViolation<Pago> violation = violations.iterator().next();
-	        assertEquals("El nombre del titular de la tarjeta no puede estar vacío", violation.getMessage());
-	    }
+	      // Verificamos que el mensaje de error es el esperado
+	      ConstraintViolation<Pago> violation = violations.iterator().next();
+	      assertEquals("El nombre del titular de la tarjeta no puede estar vacío", violation.getMessage());
+	 }
 	 
 	 
+	 
+	 /**
+	 * Prueba para validar que el número de tarjeta es inválido.
+	 * Se espera que la solicitud de pago devuelva un mensaje de error adecuado.
+	 *
+	 * @param errorMessage El mensaje de error esperado en la respuesta JSON.
+	 */
 	@Test
 	public void testNumeroTarjetaNoValido() throws Exception {
 		String errorMessage = "El número de la tarjeta no es válido";
@@ -71,7 +95,13 @@ class BugsMsPagosApplicationTests {
 				post("/pago?idUsuario=3&idEvento=1").content(tarjetaJson).contentType(MediaType.APPLICATION_JSON))
 				.andExpect(jsonPath("$.message").value(errorMessage)).andExpect(jsonPath("$.status").value("400"));
 	}
-
+	
+    /**
+     * Prueba para validar que el CVV es inválido.
+     * Se espera que la solicitud de pago devuelva un mensaje de error adecuado.
+     *
+     * @param errorMessage El mensaje de error esperado en la respuesta JSON.
+     */
 	@Test
 	public void testCVVNoValido() throws Exception {
 		String errorMessage = "El CVV no es válido";
