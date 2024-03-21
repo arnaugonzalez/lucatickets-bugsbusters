ae155b5abd2c4a276a2d4412115395e62a5236ce
mireiasuefra <mireia.suefra@gmail.com>
2024-03-21 09:45:50 +0100

    creacion de gateway

---
+++ bugs-gateway/src/main/java/bugsbusters/lucatickets/gateway/BugsGatewayApplication.java
@@ -0,0 +1,13 @@
+package bugsbusters.lucatickets.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BugsGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BugsGatewayApplication.class, args);
+	}
+
+}
